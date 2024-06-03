d0b1b30a478f345559b811ed78e3db944afefe5f
Marzena Skórska <maczenta@gmail.com>
2024-06-03 13:03:14 +0000

    deploying to heroku app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m2tca_^5er&=85oc*y^de%8c6)6qi1nl(85z!7q(5ql85k&nyw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-marzenkas-boutiqueado-k6lolxfqvm3.ws-eu110.gitpod.io',
                  '8000-marzenkas-boutiqueado-k6lolxfqvm3.ws-eu111.gitpod.io',
