55293f7813483358e301eeeed8122f50d6b2af81
Marzena Skórska <maczenta@gmail.com>
2024-06-04 16:32:07 +0000

    debug to True

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-marzenkas-boutiqueado-k6lolxfqvm3.ws-eu110.gitpod.io',
                  '8000-marzenkas-boutiqueado-k6lolxfqvm3.ws-eu111.gitpod.io',
