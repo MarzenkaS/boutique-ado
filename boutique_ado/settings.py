4305f208297d804ddc2cea9a1579dd788f289544
Marzena Skórska <maczenta@gmail.com>
2024-06-04 15:27:42 +0000

    push to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-marzenkas-boutiqueado-k6lolxfqvm3.ws-eu110.gitpod.io',
                  '8000-marzenkas-boutiqueado-k6lolxfqvm3.ws-eu111.gitpod.io',
